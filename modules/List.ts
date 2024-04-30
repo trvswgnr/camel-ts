60cdc2dc38f3938348942ca0eca2dc123f941154
Travis Wagner <travis.wagner@umusic.com>
2024-04-30 19:59:20 -0400

    add more list fns

--- modules/List.ts
+++ modules/List.ts
@@ -1,6 +1,7 @@
 import { trampoline, type thunk } from "../utils";
 import { Failure, Invalid_argument } from "./Exceptions";
 import Int, { type int } from "./Int";
+import Option, { type option } from "./Option";
 
 export type list<a> = List.Empty | List.Cons<a>;
 
@@ -69,6 +70,62 @@ namespace List {
     export const length = <a>(l: list<a>): int => length_aux(0, l);
 
     /**
+     * Compare the lengths of two lists. `compare_lengths(l1, l2)` is equivalent
+     * to `Int.compare(length(l1), length(l2))`, except that the computation
+     * stops after reaching the end of the shortest list.
+     */
+    export const compare_lengths = <a>(l1: list<a>, l2: list<a>): int => {
+        let current1 = l1;
+        let current2 = l2;
+
+        while (current1.t !== List.Empty && current2.t !== List.Empty) {
+            current1 = current1.tail;
+            current2 = current2.tail;
+        }
+
+        switch (true) {
+            case current1.t === List.Empty && current2.t === List.Empty:
+                return 0 as int; // Both lists are of the same length
+            case current1.t === List.Empty:
+                return -1 as int; // l1 is shorter than l2
+            default:
+                return 1 as int; // l1 is longer than l2
+        }
+    };
+
+    /**
+     * Compare the length of a list to an integer. `compare_length_with(l, len)` is
+     * equivalent to `compare(length(l), len)`, except that the computation
+     * stops after at most `len` iterations on the list.
+     */
+    export const compare_length_with = <a>(l: list<a>, len: int): int => {
+        let current = l;
+        let count = 0;
+
+        while (current.t !== List.Empty && count < len) {
+            current = current.tail;
+            count++;
+        }
+
+        switch (true) {
+            case current.t === List.Empty && count < len:
+                return -1 as int;
+            case current.t !== List.Empty && count === len:
+                return 1 as int;
+            default:
+                return 0 as int;
+        }
+    };
+
+    /**
+     * `is_empty(l)` is true if and only if `l` has no elements. It is
+     * equivalent to `compare_length_with(l, 0) === 0`.
+     */
+    export const is_empty = <a>(l: list<a>): l is List.Empty => {
+        return compare_length_with(l, 0 as int) === 0;
+    };
+
+    /**
      * `cons(head, tail)` returns a new list with `head` as the first element
      * and `tail` as the rest of the list.
      */
@@ -125,6 +182,45 @@ namespace List {
     };
 
     /**
+     * Return the `n`-th element of the given list. The first element (head of the
+     * list) is at position 0. Return `None` if the list is too short.
+     * @throws {Invalid_argument} if `n` is negative.
+     */
+    export const nth_opt = <a>(l: list<a>, n: int): option<a> => {
+        if (n < 0) {
+            throw new Invalid_argument("List.nth");
+        }
+        let current = l;
+        let i = n as number;
+        while (current.t !== List.Empty) {
+            if (i === 0) {
+                return Option.some(current.head);
+            }
+            i = i - 1;
+            current = current.tail;
+        }
+        return Option.none();
+    };
+
+    /**
+     * List reversal.
+     */
+    export const rev = <a>(l: list<a>): list<a> => {
+        return trampoline(rev_aux)(l, empty<a>());
+    };
+
+    /**
+     * `init len f` is [f 0; f 1; ...; f (len-1)], evaluated left to right.
+     * @throws {Invalid_argument} if `len` is negative.
+     */
+    export const init = <a>(len: int, f: (n: int) => a): list<a> => {
+        if (len < 0) {
+            throw new Invalid_argument("List.init");
+        }
+        return list(...Array(len).map((_, i) => f(i as int)));
+    };
+
+    /**
      * `append(l0, l1)` appends `l1` to `l0`. Same function as the infix
      * operator `@` (in OCaml).
      */
@@ -152,6 +248,13 @@ function append_aux<a>(l0: list<a>, l1: list<a>): thunk<list<a>> | list<a> {
         return l1;
     }
     return () => List.cons(l0.head, List.append(l0.tail, l1));
+}
+
+function rev_aux<a>(l: list<a>, acc: list<a>): thunk<list<a>> | list<a> {
+    if (l.t === List.Empty) {
+        return acc;
+    }
+    return () => List.cons(l.head, List.append(l.tail, acc));
 }
 
 export function js_array_from_list<a>(l: list<a>): a[] {
