2997ae525e831eccdc812dd4d6d645e46ff05220
Travis Wagner <travis.wagner@umusic.com>
2024-04-30 23:01:22 -0400

    expand utils

--- utils.ts
+++ utils.ts
@@ -13,10 +13,27 @@ export namespace Thunk {
     }
 }
 
-export function trampoline<T, Args extends readonly any[]>(
-    fn: (...args: Args) => T | thunk<T>,
-): (...args: Args) => T {
-    return function (...args: Args): T {
+/**
+ * The type of the arguments accepted by a function.
+ */
+export type Args<T = any> = readonly T[];
+
+/**
+ * Transforms a function that returns either a direct value or a thunk (a
+ * no-argument function that returns a value) into a function that only returns
+ * a direct value. It does this by repeatedly evaluating the function if it
+ * returns a thunk, until a direct value is obtained.
+ *
+ * @template T The type of the value to be returned by the trampoline function.
+ * @template A The type tuple representing the argument types accepted by the
+ * function `fn`.
+ * @param fn A function that takes arguments of type `Args` and returns either a
+ * value of type `T` or a thunk that returns `T`.
+ * @returns A function that takes the same arguments but guarantees to return a
+ * direct value of type `T`.
+ */
+export function tramp<T, A extends Args>(fn: Fn<T | thunk<T>, A>): Fn<T, A> {
+    return (...args: A) => {
         let result = fn(...args);
         while (Thunk.is_thunk(result)) {
             result = result();
@@ -39,12 +56,12 @@ export function with_inspect<T extends object, A>(
     return v;
 }
 
-export type fn<R = any, A extends readonly any[] = any[]> = (...args: A) => R;
+export type Fn<R = any, A extends readonly any[] = any[]> = (...args: A) => R;
 
 export type Immutable<T> = T extends object
     ? {
           readonly [K in keyof T]: T[K] extends object
-              ? T[K] extends fn
+              ? T[K] extends Fn
                   ? T[K]
                   : Immutable<T[K]>
               : T[K];
@@ -60,3 +77,23 @@ export const switch_assign = <T>(
     }
     return default_value;
 };
+
+export type Widen<T> = T extends number
+    ? number
+    : T extends boolean
+    ? boolean
+    : T extends string
+    ? string
+    : T extends bigint
+    ? bigint
+    : T extends symbol
+    ? symbol
+    : T extends undefined
+    ? undefined
+    : T extends null
+    ? null
+    : T extends object
+    ? object
+    : T;
+
+export type NoInfer<A extends any> = [A][A extends any ? 0 : never];
